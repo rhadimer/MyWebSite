c2757e546ef1e4f615a09661dcc86fe822b0fa10
Rhadimer Antigua Sánchez <rhadimer_ants@hotmail.com>
2021-03-08 15:22:49 -0400

    new-web-site
    new-web-site

---
+++ assets/js/EnviarEmail.js
@@ -0,0 +1,26 @@
+/*$(document).ready(function(){
+
+    // para asignar valor $('#name').val('Jose');
+
+ $('#formulario').submit(function (e) {
+    e.preventDefault();
+
+    var nombre = $('#name').val();
+    var email = $('#email').val();
+    var mensaje = $('#message').val();
+
+    if(nombre === ''){
+        alert('El Nombre esta vacio ');
+    } else if (email ===''){
+        alert('El Email esta vacio ');
+    } else if (mensaje ===''){
+        alert('El mensaje esta vacio')
+    }
+ 
+    
+ })
+
+});
+
+
+*/
